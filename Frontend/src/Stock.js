7beb4e43a8f0a1a53d5168879f2f409f2abfc3b6
Arpit Bhat <arpitbhat48@gmail.com>
2024-09-30 12:17:48 -0700

    Added OWnership stocks frontend

--- Frontend/src/Stock.js
+++ Frontend/src/Stock.js
@@ -4,16 +4,29 @@ import Switch from '@mui/material/Switch'; // Import a Switch component from Mat
 import './Styles/Stock.css';
 
 function Stock({ stock, isSelected, onToggle }) {
+	console.log("stocker", stock);
     return (
-        <div className="stock-item">
-            <span className="stock-name">{stock.stock_name}</span>
-            <Switch
-                checked={isSelected}  // Set the checked state based on whether the stock is selected
-                onChange={() => onToggle(stock.id)}  // Call the onToggle function with stock id
-                color="primary"
-				size="small"
-            />
-        </div>
+		<div className="stock-item">
+			<div>
+				<span className="stock-name">{stock.stock_name}</span>
+				<Switch
+					checked={isSelected}  // Set the checked state based on whether the stock is selected
+					onChange={() => onToggle(stock.id)}  // Call the onToggle function with stock id
+					color="primary"
+					size="small"
+				/>
+			</div>
+			<div>
+				<span className="stock-buttons">
+					<button className="stock-btn">+</button>
+					<button className="stock-btn">-</button>
+				</span>
+				<text>
+					Value:
+					Owned: 
+				</text>
+			</div>
+		</div>
     );
 }
 
