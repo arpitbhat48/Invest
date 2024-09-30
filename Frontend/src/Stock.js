7124b3b61b16213a39d2438b88dba403192bb18b
Arpit Bhat <arpitbhat48@gmail.com>
2024-09-29 22:33:50 -0700

    Added Stock item styles

--- Frontend/src/Stock.js
+++ Frontend/src/Stock.js
@@ -6,7 +6,7 @@ import './Styles/Stock.css';
 function Stock({ stock, isSelected, onToggle }) {
     return (
         <div className="stock-item">
-            <span>{stock.stock_name}</span>
+            <span className="stock-name">{stock.stock_name}</span>
             <Switch
                 checked={isSelected}  // Set the checked state based on whether the stock is selected
                 onChange={() => onToggle(stock.id)}  // Call the onToggle function with stock id
